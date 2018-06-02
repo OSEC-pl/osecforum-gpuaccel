97eb5a5206a9a2d792c56d0efb9caa26d8df799a
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2018-06-02 19:41:19 +0200

    Make this example somewhat less retarded.

--- src/example-matrix-multiplication/blas-matrix.c
+++ src/example-matrix-multiplication/blas-matrix.c
@@ -1,43 +1,81 @@
 #include <stdio.h>
 
+#if USE_CBLAS
+
+/* on macOS build with -framework Accelerate */
 #if __APPLE__
 #include <Accelerate/Accelerate.h>
 #elif __linux__
 #include <cblas.h>
 #endif
 
-float a[4][4] = {
-    {1.0f, 0.0f, 0.0f, 0.0f},
-    {0.0f, 1.0f, 0.0f, 0.0f},
-    {1.0f, 1.0f, 0.0f, 0.0f},
-    {0.0f, 0.0f, 1.0f, 1.0f},
-};
+#endif
 
-float b[4][4] = {
-    {1.0f, 0.0f, 0.0f, 0.0f},
-    {0.0f, 1.0f, 0.0f, 0.0f},
-    {1.0f, 1.0f, 0.0f, 0.0f},
-    {0.0f, 0.0f, 1.0f, 1.0f},
-};
+/*
+ * Allocate memory for m * n sized matrix.
+ */
+float *
+matrix_alloc(uint32_t m, uint32_t n)
+{
+	float *matrix = malloc(sizeof(float) * m * n); 
+	return matrix;
+}
 
-float c[4][4] = {
-    {0.0f, 0.0f, 0.0f, 0.0f},
-    {0.0f, 0.0f, 0.0f, 0.0f},
-    {0.0f, 0.0f, 0.0f, 0.0f},
-    {0.0f, 0.0f, 0.0f, 0.0f},
-};
+/*
+ * Fill the matrix with numbers. Not random at all.
+ */
+void
+matrix_fill(float *matrix, uint32_t m, uint32_t n)
+{
+	uint32_t i, j;
+	float f;
 
-int main() {
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4, 4, 4,
-                1.0f, (float*)a, 4, (float*)b, 4, 1.0f, (float*)c, 4);
+	i = 0, j = 0, f = 0;
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("%.2f ", c[i][j]);
-        }
-        printf("\n");
-    }
+	matrix[0] = 10;
+	matrix[1] = 10;
+	// for...	
+}
 
-    return 0;
+/*
+ * Multiply matrices.
+ */
+float *
+matrix_multiply(float *a, float *b, uint32_t m, uint32_t n)
+{
+	float *c;
+
+	c = matrix_alloc(m, n);
+
+	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 5000, 5000, 5000,
+	    1.0f, a, 5000, b, 5000, 1.0f, c, 5000);
+
+	return c;
+}
+
+int
+main()
+{
+	float *a, *b, *c;
+	uint32_t i, j, k;
+
+
+	a = matrix_alloc(5000, 5000);
+	b = matrix_alloc(5000, 5000);
+	matrix_fill(a, 5000, 5000);
+	matrix_fill(b, 5000, 5000);
+	c = matrix_multiply(a, b, 5000, 5000);
+
+	i = 0; k = 0;
+	for (j = 0; j < 5; j++) {
+		k = j * 5000;
+		for (i = 0; i < 5; i++) {
+			printf("%.2f\t", c[k]);
+			k++;
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
 
